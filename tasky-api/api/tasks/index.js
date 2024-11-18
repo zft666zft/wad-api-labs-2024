1bc303f8b922df8edabe9179a45dc95415a4589a
zft666zft <2810458566@qq.com>
2024-11-18 11:06:51 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -23,7 +23,9 @@ router.post('/', (req, res) => {
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: new Date().toISOString(), // 创建时间
+        updated_at: new Date().toISOString() 
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -37,7 +39,11 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const updatedTask = { 
+        ...tasksData.tasks[taskIndex], 
+        ...req.body, 
+        id:id,
+        updated_at: new Date().toISOString() };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
