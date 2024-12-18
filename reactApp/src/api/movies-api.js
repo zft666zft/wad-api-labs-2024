91b03a6961e4894685e67e4f8d54901977d8f5cd
zft666zft <2810458566@qq.com>
2024-12-18 19:41:13 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=b4bb00e95b13fb921a03ebee44c2aa56&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
