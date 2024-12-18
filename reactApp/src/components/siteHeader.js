6e4378e410764e87bd340d198907e33336a1c39e
zft666zft <2810458566@qq.com>
2024-12-18 19:35:22 +0000

    Integration Lab - Authentication Context and Login Page

--- reactApp/src/components/siteHeader.js
+++ reactApp/src/components/siteHeader.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/authContext";
 
 const Header = (props) => {
-  return <p>
-      Welcome to the movies!!! 
+  const context = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  return context.isAuthenticated ? (
+    <p>
+      Welcome {context.userName}! <button onClick={() => context.signout()}>Sign out</button>
     </p>
-     ;
+  ) : (
+    <p>
+      You are not logged in{" "}
+      <button onClick={() => navigate('/login')}>Login</button>
+    </p>
+  );
 };
 
 export default Header;
--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -5,6 +5,9 @@ import PublicPage from "./pages/publicPage";
 import ProfilePage from "./pages/profilePage";
 import MoviesPage from "./pages/moviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
+import LoginPage from "./pages/loginPage";
+import AuthContextProvider from "./contexts/authContext";
+import ProtectedRoutes from "./protectedRoutes";
 import Header from "./components/siteHeader";
 
 const queryClient = new QueryClient({
@@ -20,26 +23,31 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Header/>
-        <ul>
-          <li>
-            <Link to="/">Public</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={ <PublicPage /> } />
-          <Route path="/movies" element={ <MoviesPage /> } />
-          <Route path="/profile" element={ <ProfilePage /> } />
-          <Route path="*" element={ <Navigate to="/" /> } />
-        </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Header />
+          <ul>
+            <li>
+              <Link to="/">Public</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<PublicPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </AuthContextProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
